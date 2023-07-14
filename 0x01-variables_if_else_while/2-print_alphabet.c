1ff6c18f37a6f31d12288eef7b5d6a28affdbbab
kess4obehi <kessiena4excellence@gmail.com>
2023-07-14 15:36:16 -0700

    the use of putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - variables if else and while
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
