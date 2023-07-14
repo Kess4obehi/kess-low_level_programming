37a10ae9d7d5c190488b9ff78eba7d5f2cd99109
kess4obehi <kessiena4excellence@gmail.com>
2023-07-14 16:32:01 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - description of variables if else and for
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
